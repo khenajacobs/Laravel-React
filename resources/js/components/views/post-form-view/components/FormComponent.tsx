9bc00d1a44c91a18ea347205b8fc5a444af8b741
r1002128 <khena.jacobs@gmail.com>
2024-12-20 10:17:33 +0100

    add missing translation key

--- resources/js/components/views/post-form-view/components/FormComponent.tsx
+++ resources/js/components/views/post-form-view/components/FormComponent.tsx
@@ -50,7 +50,7 @@ const FormComponent: React.FC<FormComponentProps> = ({ onSubmit }) => {
       </div>
 
       <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-700 transition duration-300">
-        {t('form.submit')}
+        {t('postForm.submit')}
       </button>
     </form>
   );
