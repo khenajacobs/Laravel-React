d6ddb90e9cc978aede8245f12b018a8bb2c30a16
r1002128 <khena.jacobs@gmail.com>
2024-12-20 10:19:18 +0100

    update missing translation keys

--- resources/js/components/views/post-form-view/post-form.schema.ts
+++ resources/js/components/views/post-form-view/post-form.schema.ts
@@ -3,9 +3,9 @@ import { z } from 'zod';
 import i18n from 'i18next';
 
 export const formSchema = z.object({
-  name: z.string().min(1, i18n.t('form.nameRequired')),
-  email: z.string().email(i18n.t('form.emailInvalid')),
-  message: z.string().min(1, i18n.t('form.messageRequired')),
+  name: z.string().min(1, i18n.t('postForm.nameRequired')),
+  email: z.string().email(i18n.t('postForm.emailInvalid')),
+  message: z.string().min(1, i18n.t('postForm.messageRequired')),
 });
 
 export type FormSchema = z.infer<typeof formSchema>;
